20aea965f1b13a5758b20eec67c58708945120ab
david <marc.temstet88@gmail.com>
2026-04-16 17:54:05 +0200

    sdv bh

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://chefruth.fr";
 
 export default function robots(): MetadataRoute.Robots {
